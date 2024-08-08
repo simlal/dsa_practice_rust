c69553ed94c25009e6f5c7068b9911d10a85fa58
Simon Lalonde <simlalonde@hotmail.com>
2024-08-08 15:29:58 -0400

    Base commit main launcher for each challenge.rs files

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+// modules for each problem
+mod remove_duplicates_sorted_array;
+
+fn list_challenges(main_dir: &Path) {
+    match fs::read_dir(main_dir) {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(entry) => {
+                        if entry.file_name() == "main.rs" {
+                            continue;
+                        }
+                        if let Some(file_stem) = entry.path().file_stem() {
+                            println!("{}", file_stem.to_string_lossy());
+                        }
+                    },
+                    Err(e) => println!("Error: {}", e)
+                }
+            }
+        },
+        Err(e) => println!("Error: {}", e)
+    }
+}
+
+struct HelpMessage<'a> {
+    base_message: &'a str,
+    program_name: &'a str
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let problem_to_run = args.get(1);
+
+    let help_msg = HelpMessage {
+        base_message: "Usage: {} <challenge_name>\n\
+        If challenge exists, make sure to:\n\
+        - Create a new <challenge_name.rs> file\n\
+        - Write pub fn run() in it\n\
+        - Implement the challenge logic",
+        program_name: &args[0],
+    };
+
+    match problem_to_run {
+        Some(challenge) => match challenge.as_str() {
+            "--help" => println!(
+                "{}", help_msg.base_message.replace("{}", help_msg.program_name)
+            ),
+            "remove_duplicates_sorted_array" => {
+                println!("Running: <{}>...", challenge);
+                remove_duplicates_sorted_array::run();
+            }
+            other => {
+                println!("Unknown challenge <{}>", other);
+                println!("Possible challenges are:");
+                list_challenges(Path::new(file!()).parent().unwrap());
+            }
+        },
+        None => println!(
+            "{}", help_msg.base_message.replace("{}", help_msg.program_name)
+        ),
+    }
+}
