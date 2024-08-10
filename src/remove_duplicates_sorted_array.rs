1aa3d6851b3f5b7692278a3c012b88de41ef4277
Simon Lalonde <simlalonde@hotmail.com>
2024-08-10 10:50:49 -0400

    Fix remove_dups

--- src/remove_duplicates_sorted_array.rs
+++ src/remove_duplicates_sorted_array.rs
@@ -3,23 +3,26 @@ struct Solution;
 
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-        let mut num_duplicates = 0;
-        for i in 0..nums.len() {
-            for j in i..nums.len() {
-                if i != j && nums[i] == nums[j] {
-                    // println!("{} is equal to {}", nums[i], nums[j]);
-                    num_duplicates += 1;
-                }
+        let mut left = 1;
+        for right in 1..nums.len() {
+            if nums[right] != nums[right - 1] {
+                nums[left] = nums[right];
+                left += 1;
             }
         }
-        num_duplicates
+
+        left as i32
     }
 }
 pub fn run() {
     println!("Challenge file name: {}\n", file!());
     println!("########################  SOLUTION-DEBUGGING-START  ########################\n");
-    let mut my_vec = vec![1, 2, 3, 4, 2];
-    let result = Solution::remove_duplicates(&mut my_vec);
+    
+    // let mut my_vec = vec![0,0,1,1,1,2,2,3,3,4];
+    let mut vec2 = vec![1,1,2];
+    let result = Solution::remove_duplicates(&mut vec2);
     println!("Number of duplicates is: {}", result);
+    println!("Mutated vec: {:?}", vec2);
+    
     println!("\n#######################  SOLUTION-DEBUGGING-END    ########################");
 }
